62c353d0e88ee82eb2ef96b82d86b624dacaa455
jameshoutw <152400413+jameshoutw@users.noreply.github.com>
2026-01-21 16:18:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活
 const FILE_PATH = process.env.FILE_PATH || '.tmp';    // 运行目录,sub节点文件保存目录
 const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
-const UUID = process.env.UUID || '84705c0d-5036-44b1-a07e-d1582e653595'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+const UUID = process.env.UUID || '985d7402-7861-4504-ae58-dec351956309'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
-const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'galaxy.tuma28.indevs.in';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOTgxYjUwMzY1MWQ4MmNiOTJjMTgwZTI1ZWYwOThkYWYiLCJ0IjoiMWJkNDM0ZGEtMzA4Yy00ZTE3LTlmZGUtYTM2Y2U4MmY1MGZjIiwicyI6IlpqVTBNbVUzWXpRdE5HRmtOQzAwTnpKaExUaGhPR0l0WTJJM09URmtPVFkzTldWaSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_PORT = process.env.ARGO_PORT || 24000;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'Galaxy';                  // 节点名称
+const NAME = process.env.NAME || 'Galaxycp0121';                  // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
@@ -625,3 +625,4 @@ app.get("/", async function(req, res) {
 });
 
 app.listen(PORT, () => console.log(`http server is running on port:${PORT}!`));
+
